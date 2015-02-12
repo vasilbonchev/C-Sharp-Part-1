ce5db3954758d99df98b5bc3e68233ddad0fb038
Vasil Vasilev <vasil.bonchev.vasilev@gmail.com>
2015-02-12 23:45:45 +0200

    loops

---
+++ 14.Decimal to Binary Number/Decimal to Binary Number.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+//Using loops write a program that converts an integer number to its binary representation.
+
+
+
+class DecimalBinary
+{
+    static void Main()
+    {
+        Console.Write("decimal: ");
+        int number = int.Parse(Console.ReadLine());
+        string binary = string.Empty;
+
+        if (number == 0)
+        {
+            Console.WriteLine("0");
+            return;
+        }
+
+        do
+        {
+            int bit = number % 2;
+            number /= 2;
+            binary = Convert.ToString(bit) + binary;
+        }
+        while (number > 1);
+
+        binary = Convert.ToString(number) + binary;
+        Console.WriteLine(binary);
+    }
+}
+
