33ef9f8bb0f76378fd9865f735c2f3e86ec2a987
Vasil Vasilev <vasil.bonchev.vasilev@gmail.com>
2015-02-12 23:44:54 +0200

    loops

---
+++ 13.Binary to Decimal/Binary to Decimal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+//Using loops write a program that converts a binary integer number to its decimal form.
+//The input is entered as string. The output should be a variable of type long.
+//Do not use the built-in .NET functionality.
+
+
+
+class BinaryToDecimal
+{
+    static void Main()
+    {
+        Console.Write("Binary: ");
+        string binary = Console.ReadLine();
+        int iter = binary.Length;
+        int decimalN = 0;
+
+        for (int i = 0; i < iter; i++)
+			{
+                int index = binary.Length - 1;
+                string bit = binary.Substring(index, 1);
+                decimalN += (int.Parse(bit)) * ((int)Math.Pow(2, i));
+                binary = binary.Substring(0, binary.Length - 1);
+
+			}
+        Console.WriteLine(decimalN);
+    }
+}
+
