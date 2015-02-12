9aace57597c59b37bc8fea9a96e4341c0ed15c6f
Vasil Vasilev <vasil.bonchev.vasilev@gmail.com>
2015-02-12 23:32:32 +0200

    hh

---
+++ 01.Numbers from 1 to N/Numbers 1 to N.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+//Write a program that enters from the console a positive integer n and prints all the numbers from 1 to n, on a single line, separated by a space.//
+
+
+
+
+class Numbers1ToN
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int a = n;
+        for (int i = 1; i <= a; i++)
+        {
+            Console.Write(i + " ");
+        }
+        
+    }
+}
+
