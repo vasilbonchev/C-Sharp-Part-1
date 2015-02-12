6f0d082cf68ecdd5b79a8ffd1632eeb256e9ed70
Vasil Vasilev <vasil.bonchev.vasilev@gmail.com>
2015-02-12 23:39:13 +0200

    loops

---
+++ 06.Calculate NK/NK.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Numerics;
+
+//Write a program that calculates n! / k! for given n and k (1 < k < n < 100).//
+
+
+
+
+class CalculateNK
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int k = int.Parse(Console.ReadLine());
+        int factorielN = 1;
+        int factorielk = 1;
+
+        for (int i = 1; i <= n; i++)
+        {
+            factorielN *= i;
+            if (i <= k)
+            {
+                factorielk *= i;
+            }
+        }
+        Console.WriteLine(factorielN / factorielk);
+    }
+}
+
