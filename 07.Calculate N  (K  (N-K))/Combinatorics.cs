8e7512f29d7e4b9d97bb95d649136283d3af0017
Vasil Vasilev <vasil.bonchev.vasilev@gmail.com>
2015-02-12 23:40:13 +0200

    loops

---
+++ 07.Calculate N  (K  (N-K))/Combinatorics.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Numerics;
+
+//In combinatorics, the number of ways to choose k different members out of a group of n different elements (also known as the number of combinations) is calculated by the following formula: formula For example, there are 2598960 ways to withdraw 5 cards out of a standard deck of 52 cards.
+
+
+
+
+class Math
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int k = int.Parse(Console.ReadLine());
+
+        int difference = n - k;
+
+        BigInteger factorielN = 1;
+        BigInteger factorielK = 1;
+        BigInteger diffFactorial = 1;
+
+        for (int i = 1; i <= n; i++)
+        {
+            factorielN *= i;
+
+            if (i <= k)
+            {
+                factorielK *= i;
+            }
+
+            if (difference >= i)
+            {
+                diffFactorial *= i;
+            }
+        }
+
+        Console.WriteLine(factorielN / (factorielK * diffFactorial));
+
+    }
+}
+
