196b1fae91b884ae5a93ab6ec4f193ceae5e07e2
Vasil Vasilev <vasil.bonchev.vasilev@gmail.com>
2015-02-12 23:52:26 +0200

    game

---
+++ Game/Game.cs
@@ -0,0 +1,57 @@
+﻿using System;
+class RandomNumbers
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Random rnd = new Random();
+            int secretNumber = rnd.Next(1, 1000);
+            int counterHodove = 0;
+            while (true)
+            {
+                counterHodove++;
+                Console.Write("Insert number: ");
+                // variable scope - vidimost na promenlivata
+                int number = 0;
+                try
+                {
+                    number = int.Parse(Console.ReadLine());
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine(e.Message);
+                    continue;
+                }
+
+                Console.Clear();
+
+                if (number < secretNumber)
+                {
+                    Console.WriteLine("Up");
+                }
+                else if (number > secretNumber)
+                {
+                    Console.WriteLine("Down");
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine("You guess! The number is: " + secretNumber + " You win with: " + counterHodove + " hoda");
+
+            Console.WriteLine("One more game? (yes) or tap any key to terminate");
+            String responseNewGame = Console.ReadLine();
+            if (responseNewGame.ToUpper().Equals("Yes".ToUpper()))
+            {
+                continue;
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+}
