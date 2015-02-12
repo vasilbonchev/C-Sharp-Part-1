92871e26a68c46650a5aeddb9a7fe4d97a9b5d1c
Vasil Vasilev <vasil.bonchev.vasilev@gmail.com>
2015-02-12 23:43:15 +0200

    loops

---
+++ 09.Matrix of Numbers/Matrix.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+//Write a program that reads from the console a positive integer number n (1 ≤ n ≤ 20) and prints a matrix like in the examples below. Use two nested loops.
+
+
+
+class Matrix
+{
+    static void Main()
+    {
+        int number = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < number; i++)
+        {
+            int counter = 1;
+            while (counter <= number)
+            {
+                Console.Write("{0} ", counter + i);
+                counter++;
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
