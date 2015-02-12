3035bb4ab86e0baf3bd7c3139bfe402eec6803f9
Vasil Vasilev <vasil.bonchev.vasilev@gmail.com>
2015-02-12 23:42:22 +0200

    loops

---
+++ 08.Catalan Numbers/Catalan Numbers.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Numerics;
+
+//In combinatorics, the Catalan numbers are calculated by the following formula: 
+//Write a program to calculate the nth Catalan number by given n (0 ≤ n ≤ 100).
+
+
+class CatalanNumbers
+{
+    static void Main()
+    {
+        BigInteger number = BigInteger.Parse(Console.ReadLine());
+        BigInteger numberFactoriel = 1;
+
+        BigInteger numberIncremented = number + 1;
+        BigInteger incrementedFactorial = 1;
+
+        BigInteger doubleNumber = number * 2;
+        BigInteger doubleFactoriel = 1;
+
+        int counter = 1;
+
+        while (counter <= number)
+        {
+            numberFactoriel *= counter;
+            counter++;
+        }
+
+        counter = 1;
+        while (counter <= numberIncremented)
+        {
+            incrementedFactorial *= counter;
+            counter++;
+        }
+
+        counter = 1;
+        while (counter <= doubleNumber)
+        {
+            doubleFactoriel *= counter;
+            counter++;
+        }
+
+        BigInteger catalan = doubleFactoriel / (incrementedFactorial * numberFactoriel);
+
+        Console.WriteLine("{0}", catalan);     
+    }
+}
+
