9e22ae10f581908bd70a3ece1daebd76d9526b3c
Vasil Vasilev <vasil.bonchev.vasilev@gmail.com>
2015-02-12 23:47:28 +0200

    loops

---
+++ 16.Decimal to Hexadecimal/Decimal to Hexadecimal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+//Using loops write a program that converts an integer number to its hexadecimal representation.
+
+
+
+class  DecToHex
+{
+    static void Main()
+    {
+        Console.Write("number: ");
+        long number = long.Parse(Console.ReadLine());
+        string hexadecimal = string.Empty;
+        long inputNumber = number;
+        string[] hexes = new string[16] { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F" };
+
+        while (number >= 16)
+        {
+            hexadecimal = hexes[(number % 16)] + hexadecimal;
+            number /= 16;
+        }
+
+        hexadecimal = hexes[number] + hexadecimal;
+        Console.WriteLine(hexadecimal);
+    }
+}
+
